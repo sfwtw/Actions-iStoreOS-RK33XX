9d1ed57ccf4e5742f3ebef05393ae0d430f331ca
windrua <34590060+sfwtw@users.noreply.github.com>
2024-11-06 13:56:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,10 @@ date_version=$(date +"%Y%m%d%H")
 echo $date_version > version
 
 # 为iStoreOS固件版本加上编译作者
-author="xiaomeng9597"
+author="sfwtw"
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
 
 # 拉取我的软件包仓库
-sed -i '$a src-git xmpackage https://github.com/xiaomeng9597/openwrt-packages2.git;main' feeds.conf.default
+# sed -i '$a src-git xmpackage https://github.com/xiaomeng9597/openwrt-packages2.git;main' feeds.conf.default
+sed -i '$a src-git modem https://github.com/Siriling/5G-Modem-Support.git;main' feeds.conf.default
